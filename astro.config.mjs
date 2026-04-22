5e67562528d0ac7bb48ac621bcc69c2e194cf325
alex-hiermann <alex.hiermann@gmx.at>
2026-04-22 20:36:03 +0200

    Switch to fitmitbenedikt.at

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://btrcka.at',
+  site: 'https://fitmitbenedikt.at',
   base: '/',
   integrations: [sitemap()],
   vite: {
